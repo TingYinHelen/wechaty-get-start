94ef2d77a428aae5270b0b8efcd7ac28e1d7fb7e
Helen <yinhelen.hlj@qq.com>
2018-06-09 20:31:58 +0800

    添加图灵机器人接口

--- mybot.js
+++ mybot.js
@@ -1,5 +1,10 @@
 const { Wechaty, Room } = require('wechaty')
-console.log('=======')
+const request = require('request');
+
+const tulingUrl = 'http://openapi.tuling123.com/openapi/api/v2';
+const API_KEY = "9bcdb081db0c4ea18b8890c471ba21b7";
+const USER_ID = '276757';
+
 Wechaty.instance()
 	.on('scan', (url, code) => {
 		if (!/201|200/.test(String(code))) {
@@ -21,19 +26,60 @@ Wechaty.instance()
 	})
 
 	.on('message', async function (m) {
+        if (m.self()) {
+			return;
+		}
 		const contact = m.from()
 		const content = m.content()
-		const room = m.room()
+        const room = m.room()
 
-		if (room) {
-			console.log(`Room: ${room.topic()} Contact: ${contact.name()} Content: ${content}`)
-		} else {
-			console.log(`Contact: ${contact.name()} Content: ${content}`)
-		}
+        let requestData = {
+            reqType: 0,
+            perception: {
+                inputText: {
+                    text: content
+                }
+            },
+            userInfo: {
+                apiKey: API_KEY,
+                userId: USER_ID
+            }
+        };
 
-		if (m.self()) {
-			return
-		}
+
+		if (room && room.topic() === 'Chatbot') {
+            request({
+                url: tulingUrl,
+                method: "POST",
+                json: true,
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: requestData
+            }, (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    let content = body.results[0].values.text;
+                    console.log('content=====', content);
+                    m.say(content);
+                }
+            });
+            console.log(`Room: ${room.topic()} Contact: ${contact.name()} Content: ${content}`)
+        } else {
+            request({
+                url: tulingUrl,
+                method: "POST",
+                json: true,
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: requestData
+            }, (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    let content = body.results[0].values.text
+                    console.log('content', content);
+                }
+            });
+        }
 
 		if (/hello/.test(content)) {
 			m.say("hello how are you")
@@ -56,4 +102,4 @@ Wechaty.instance()
 		}
 	})
 
-	.init()
+	.start()
